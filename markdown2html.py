eb08bdfa24cc8d7d4ab989c850c407de5c9443c0
Felipe Gutierrez <1560@holbertonschool.com>
2021-02-11 19:38:35 -0500

    Add: adding task file

---
+++ markdown2html.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Markdown to HTML """
+import sys
+import os.path
+
+
+if __name__ == "__main__":
+    """ Function that takes an argument 2 strings """
+    if len(sys.argv) < 2:
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
+        exit(1)
+    elif os.path.isfile(sys.argv[1]) is False:
+                print("Missing {}\n".format(sys.argv[1]), file=sys.stderr)
+                exit(1)
+    else:
+        exit(0)
